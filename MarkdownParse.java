7238867aa12aa7e2358fc5e596561f68140069d3
yuz120 <yuz120@ucsd.edu>
2022-04-22 12:59:33 -0700

    Error from running junit
    amandadeMacBook-Air:markdown-parser-my amanda$ javac -cp "junit-4.12.jar;hamcrest-core-1.3.jar;." MarkdownParseTest.java
    MarkdownParseTest.java:1: error: package org.junit does not exist
    import static org.junit.Assert.*; //imports junit assert
                           ^
    MarkdownParseTest.java:2: error: package org.junit does not exist
    import org.junit.*; //imports junit
    ^
    MarkdownParseTest.java:4: error: cannot find symbol
        @Test
         ^
      symbol:   class Test
      location: class MarkdownParseTest
    MarkdownParseTest.java:6: error: cannot find symbol
            assertEquals(2,1+1); //asserts 2 equals to 1+1
            ^
      symbol:   method assertEquals(int,int)
      location: class MarkdownParseTest
    4 errors

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,7 +15,7 @@ public class MarkdownParse {
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
-            if (closeBracket = markdown.indexOf("]", openBracket){
+            //if (closeBracket = markdown.indexOf("]", openBracket){
                 
             }//
             int openParen = markdown.indexOf("(", closeBracket);
---
+++ MarkdownParseTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*; //imports junit assert
+import org.junit.*; //imports junit 
+public class MarkdownParseTest{ //tester class for MarkdownParse.java
+    @Test
+    public void addition(){  //test addition in MarkdownParseTest
+        assertEquals(2,1+1); //asserts 2 equals to 1+1
+    }
+}
