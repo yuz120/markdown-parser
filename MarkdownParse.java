856ef6de7fa07d2521b170cd076a5e290ee56db3
yuz120 <yuz120@ucsd.edu>
2022-04-24 23:11:33 -0700

    trial 2: count the pairs of brackets and parenthesis
    amandadeMacBook-Air:markdown-parser-my amanda$ javac MarkdownParse.java
    amandadeMacBook-Air:markdown-parser-my amanda$ java MarkdownParse testfile2.md
    initial currentIndex is 0
    After while loop, the currentIndex is 0
    [https://www.healthline.com/nutrition/top-evidence-based-health-benefits-of-coffee]
    amandadeMacBook-Air:markdown-parser-my amanda$ javac MarkdownParse.java
    amandadeMacBook-Air:markdown-parser-my amanda$ java MarkdownParse test-file.md
    initial currentIndex is 0
    After while loop, the currentIndex is 0
    [https://something.com]
    amandadeMacBook-Air:markdown-parser-my amanda$

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,20 +11,30 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        int numOpenBracket=0;
+        int numCloseBracket=0;
+        int numOpenParen=0;
+        int numCloseParen=0;
         System.out.println("initial currentIndex is " +currentIndex);
         while(currentIndex < markdown.length()) { 
             int openBracket = markdown.indexOf("[", currentIndex);
+            numOpenBracket++;
             int closeBracket = markdown.indexOf("]", openBracket);
+            numOpenBracket++;
             int openParen = markdown.indexOf("(", closeBracket);
+            numOpenParen++;
             int closeParen = markdown.indexOf(")", openParen);
+            numCloseParen++;
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            
+            if(numCloseBracket!=numOpenBracket || numCloseParen!=numOpenParen){
+                break;
+            }
 
             currentIndex = closeParen+1;
             System.out.println("After incrementing  currentIndex is " +currentIndex);
-            //int secondCloseParen = markdown.indexOf(")", currentIndex);
-            if(closeParen<markdown.length()){
-                break;
-            }
+            
+        
             
            
         }
