0a7757fa1a304e893292c89d3e79595d88fbb600
yuz120 <103283467+yuz120@users.noreply.github.com>
2022-05-06 12:29:08 -0700

    Create makefile.java

---
+++ makefile.java
@@ -0,0 +1,48 @@
+//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class MarkdownParse {
+
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then read link upto next )
+        int currentIndex = 0;
+        while(currentIndex < markdown.length()) {
+            int openBracket = markdown.indexOf("[", currentIndex);
+            //No links left to be found exit
+            if (openBracket == -1) {
+                break;
+            }
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            //no close paren so there is no link, ensures no infinite loop
+            if (openParen == -1) {
+                break;
+            }
+            int closeParen = markdown.indexOf(")", openParen);
+            if (markdown.charAt(openBracket-1)== '!') {
+                currentIndex = closeParen + 1;
+            }
+            else {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+        }
+        if (toReturn.size() == 0) {
+            System.out.println("No links found");
+        }
+        return toReturn;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+	    System.out.println(links);
+    }
+}
